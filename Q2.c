56aebf4f4dc9163581c8632d577568e19e5fc5ef
22103335 <22103335@mail.jiit.ac.in>
2024-08-02 21:02:05 +0530

    Add files via upload

--- Q2.c
+++ Q2.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
 void main() {
-    int m; 
-    printf("Enter the number of rows ");
-    scanf("%d", &m);
-    int a[m][m];
-    for (int i = 0; i < m; i++)
-        for (int j = 0; j < m; j++) {
-            printf("Enter the element ");
-            scanf("%d", &a[i][j]);
-        }
-    for (int i = 0; i < m; i++)
-        for (int j = i + 1; j < m; j++)
-            (a[i][j] += a[j][i]), (a[j][i] = a[i][j] - a[j][i]), (a[i][j] -= a[j][i]);
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++)
-            printf("%d ", a[i][j]);
-            printf("\n");
-    }
+    int n;
+    printf("Enter the number of elements ");
+    scanf("%d",&n);
+    int a[n];
+    float s = 0;
+    for (int i = 0; i < n; i++)
+        s += i;
+    printf("The average is %d", s / n);
 }
